b934b7f91263aff53cf37b1e61fd36bf7853a845
shengxianshui <shengxianshui@imorpheusai.io>
2020-10-08 11:27:27 +0800

    Extend 'put' API for multi sessions

--- src/main/java/io/tau/DhtShell.java
+++ src/main/java/io/tau/DhtShell.java
@@ -5,6 +5,7 @@ import com.frostwire.jlibtorrent.alerts.*;
 import com.frostwire.jlibtorrent.swig.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -22,6 +23,7 @@ public final class DhtShell {
 
     // command list for help doc
     private static final String PutImmutableItem = "put <string value>: put immutable item";
+    private static final String MSPutImmutableItem = "msput <string value>: put immutable item for all sessions";
     private static final String GetImmutableItem = "get <sha1>: get immutable item";
 
     private static final String GetPeers = "get_peers <sha1>: get peers which store sha1 value";
@@ -29,6 +31,7 @@ public final class DhtShell {
     private static final String GenKeyPair = "mkeys: generate ed25519 key pair";
 
     private static final String PutMutableItem = "mput <pub-key> <priv-key> <string value> <salt>: put mutable item";
+   private static final String MSPutMutableItem = "msmput <pub-key> <priv-key> <string value> <salt>: put mutable item for all sessions";
     private static final String GutMutableItem = "mget <pub-key> <salt>: get mutable item";
 
     private static final String Count_Nodes = "count_nodes: count nodes of dht";
@@ -37,15 +40,22 @@ public final class DhtShell {
 
     private static final String List_nodes = "list_nodes: list dht nodes";
 
+    private static final String Compress = "compress <rand bytes size> <test accounts>: compress tests";
+
     private static final String Quit = "quit: exit this application";
 
     private static final String Help = "commands list:" + "\n"
-            + PutImmutableItem + "\n" + GetImmutableItem + "\n"
-	    + GetPeers + "\n" + GenKeyPair + "\n"
-            + PutMutableItem + "\n" + GutMutableItem + "\n"
-	    + Count_Nodes + "\n" + Put_Bomb + "\n" + List_nodes + "\n" + Quit + "\n";
+            + PutImmutableItem + "\n" + MSPutImmutableItem  + "\n" + GetImmutableItem + "\n"
+	        + GetPeers + "\n" + GenKeyPair + "\n"
+            + PutMutableItem + "\n" + MSPutMutableItem + "\n" + GutMutableItem + "\n"
+	        + Count_Nodes + "\n" + Put_Bomb + "\n" + List_nodes + "\n"
+            + Compress + "\n" + Quit + "\n";
 
     private static final SimpleDateFormat LogTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private static final String sUndefinedEntry = entry.data_type.undefined_t.toString();
+
+    private static final int Sessions_Count = 32;
 
     public static void main(String[] args) throws Throwable {
 
@@ -69,52 +79,69 @@ public final class DhtShell {
                     log(a.message());
                 }
 
-		if (type == AlertType.INCOMING_CONNECTION) {
-		    IncomingConnectionAlert a = (IncomingConnectionAlert) alert;
-		    log(a.message());
-		}
+		        if (type == AlertType.DHT_MUTABLE_ITEM) {
+		            DhtMutableItemAlert a = (DhtMutableItemAlert) alert;
+		            log(a.message());
+		        }
 
-		if (type == AlertType.DHT_MUTABLE_ITEM) {
-		     DhtMutableItemAlert a = (DhtMutableItemAlert) alert;
-		     log(a.message());
-		}
-
-		if (type == AlertType.DHT_IMMUTABLE_ITEM) {
-		     DhtImmutableItemAlert a = (DhtImmutableItemAlert) alert;
-		     log(a.message());
-		}
+		        if (type == AlertType.DHT_IMMUTABLE_ITEM) {
+		            DhtImmutableItemAlert a = (DhtImmutableItemAlert) alert;
+		            log(a.message());
+		        }
 
                 if (type == AlertType.DHT_PUT) {
                     DhtPutAlert a = (DhtPutAlert) alert;
                     log(a.message());
                 }
-
-		if (type == AlertType.DHT_LOG) {
-		    DhtLogAlert a = (DhtLogAlert) alert;
-		    log(a.message());
-		}
             }
         };
 
-        SessionManager s = new SessionManager();
-        s.addListener(mainListener);
-        s.start(SessionSettings.getTauSessionParams());
+        List<SessionManager> sessions = new ArrayList<>();
+        for (int i = 0; i < Sessions_Count; i++) {
+                SessionManager session = new SessionManager();
+                session.addListener(mainListener);
+                session.start(SessionSettings.getTauSessionParams());
+                sessions.add(session);
+        }
+
+        SessionManager s = sessions.get(0);
+
+        /*
+        Thread getThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                print("wait for 10m...");
+                try {
+                    Thread.sleep(600 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                getbomb(s, "getbomb nihao_nancha");
+            }
+        });
+        getThread.start();
+        */
 
         Scanner in = new Scanner(System.in);
-	System.out.println("dht shell is ready");
+	    System.out.println("dht shell is ready");
         while (in.hasNextLine()) {
             //System.out.print("$ ");
 
             String line = in.nextLine().trim();
 
-	    if (is_help(line)) {
+	        if (is_help(line)) {
                 print(Help);
-	    } else if (is_quit(line)) {
+	        } else if (is_quit(line)) {
                 quit(s);
             } else if (is_put(line)) {
                 put(s, line);
+            } else if (is_msput(line)) {
+                msput(sessions, line);
             } else if (is_get(line)) {
                 get(s, line);
+            } else if (is_getbomb(line)) {
+                getbomb(s, line);
             } else if (is_get_peers(line)) {
                 get_peers(s, line);
             } else if (is_announce(line)) {
@@ -123,6 +150,8 @@ public final class DhtShell {
                 mkeys(line);
             } else if (is_mput(line)) {
                 mput(s, line);
+            } else if (is_msmput(line)) {
+                msmput(sessions, line);
             } else if (is_mget(line)) {
                 mget(s, line);
             } else if (is_magnet(line)) {
@@ -133,6 +162,14 @@ public final class DhtShell {
                 putBomb(s);
             } else if (is_list_nodes(line)) {
                 list_nodes(s);
+            } else if (is_pause(line)) {
+                pause(s);
+            } else if (is_resume(line)) {
+                resume(s);
+            } else if (is_applySettings(line)) {
+                applySettings(s);
+            } else if (is_compress(line)) {
+                compress(s, line);
             } else if (is_invalid(line)) {
                 invalid(line);
             }
@@ -182,26 +219,36 @@ public final class DhtShell {
         print("Wait for completion of put for key: " + sha1);
     }
 
+    private static boolean is_msput(String s) {
+        return s.startsWith("msput ");
+    }
+
+    private static void msput(List<SessionManager> smList, String s) {
+        for (SessionManager sm : smList) {
+            put(sm, s);
+        }
+    }
+
     private static boolean is_putbomb(String s) {
         return s.startsWith("putbomb"); 
     }
 
     private static void putBomb(SessionManager sm) {
-	print("starting put loop bombing......");
+	    print("starting put loop bombing......");
         int i = 0;
-	int loop = 1000000;
-	while (i < loop) {
+	    int loop = 1000000;
+	    while (i < loop) {
             String sha1 = sm.dhtPutItem(new Entry(String.valueOf(i))).toString();
-	    print("Wait for completion of put for key: " + sha1 + ", value:" + i);
-	    i++;
+	        print("Wait for completion of put for key: " + sha1 + ", value:" + i);
+	        i++;
 
-	    try {
-	        Thread.sleep(60 * 1000);
-	    } catch (InterruptedException e) {
-	        e.printStackTrace();
+	        try {
+	            Thread.sleep(60 * 1000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
 	    }
-	}
-	print("ending put loop bombing......");
+	    print("ending put loop bombing......");
     }
 
     private static boolean is_get(String s) {
@@ -213,6 +260,44 @@ public final class DhtShell {
         print("Waiting a max of 20 seconds to get data for key: " + sha1);
         Entry data = sm.dhtGetItem(new Sha1Hash(sha1), 20);
         print(data.toString());
+    }
+
+    private static boolean is_getbomb(String s) {
+        return s.startsWith("getbomb");
+    }
+
+    private static void getbomb(SessionManager sm, String s) {
+        String data = s.split(" ")[1];
+        String sha1 = sm.dhtPutItem(new Entry(data)).toString();
+        print("Wait for completion of put for key: " + sha1);
+
+        try {
+            Thread.sleep(60 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        print("get bombing...");
+        int i = 0;
+        Entry result = null;
+        while (i < 100) {
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            result = sm.dhtGetItem(new Sha1Hash(sha1), 20);
+            if (result == null || isEntryUndefined(result)) {
+                print("getbomb failed");
+            } else {
+                print("getbomb success");
+            }
+
+            i++;
+        }
+
+        print("getbomb is ending");
     }
 
     private static boolean is_get_peers(String s) {
@@ -267,13 +352,23 @@ public final class DhtShell {
         byte[] publicKey = Utils.fromHex(arr[1]);
         byte[] privateKey = Utils.fromHex(arr[2]);
         String data = arr[3];
-	byte[] salt = null;
-	if (arr.length > 4) {
+	    byte[] salt = null;
+	    if (arr.length > 4) {
             salt = arr[4].getBytes();
-	}
+	    }
         sm.dhtPutItem(publicKey, privateKey, new Entry(data),
                 salt == null ? new byte[0] : salt);
         print("Wait for completion of mput for public key: " + arr[1]);
+    }
+
+    private static boolean is_msmput(String s) {
+        return s.startsWith("msmput ");
+    }
+
+    private static void msmput(List<SessionManager> smList, String s) {
+        for (SessionManager sm : smList) {
+            mput(sm, s);
+        }
     }
 
     private static boolean is_mget(String s) {
@@ -283,10 +378,10 @@ public final class DhtShell {
     private static void mget(SessionManager sm, String s) {
         String[] arr = s.split(" ");
         byte[] publicKey = Utils.fromHex(arr[1]);
-	byte[] salt = null;
-	if (arr.length > 2) {
-	    salt = arr[2].getBytes();
-	}
+	    byte[] salt = null;
+	    if (arr.length > 2) {
+	        salt = arr[2].getBytes();
+	    }
 
         print("Waiting a max of 20 seconds to get mutable data for public key: " + arr[1]);
         SessionManager.MutableItem data = sm.dhtGetItem(publicKey,
@@ -312,6 +407,43 @@ public final class DhtShell {
 
     private static void count_nodes(SessionManager s) {
         log("DHT contains " + s.stats().dhtNodes() + " nodes");
+    }
+
+    private static boolean is_pause(String s) {
+        return s.startsWith("pause");
+    }
+
+    private static void pause(SessionManager s) {
+        s.pause();
+        log("dht paused");
+    }
+
+    private static boolean is_resume(String s) {
+        return s.startsWith("resume");
+    }
+
+    private static void resume(SessionManager s) {
+        s.resume();
+        log("dht resumed");
+    }
+
+    private static boolean is_applySettings(String s) {
+        return s.startsWith("applySettings");
+    }
+
+    private static void applySettings(SessionManager s) {
+
+        SettingsPack sp = s.settings();
+        sp.swig().set_str(
+                settings_pack.string_types.listen_interfaces.swigValue(),
+                "0.0.0.0:8080");
+        sp.swig().set_str(settings_pack.string_types.dht_bootstrap_nodes.swigValue(),
+                SessionSettings.dhtBootstrapNodes());
+        sp.swig().set_bool(settings_pack.bool_types.enable_ip_notifier.swigValue(), false);
+
+        new SessionHandle(s.swig()).reopenNetworkSockets();
+
+        print("apply settings");
     }
 
     private static boolean is_list_nodes(String s) {
@@ -355,6 +487,36 @@ public final class DhtShell {
         }
     }
 
+    private static boolean is_compress(String s) {
+        return s.startsWith("compress ");
+    }
+
+    private static void compress(SessionManager sm, String s) {
+        String[] arguments = s.split(" ");
+
+        int bytes_size = 1000;
+        int loop_account = 1;
+
+        if (arguments.length == 2) {
+            bytes_size = Integer.parseInt(arguments[1]);
+        } else if (arguments.length == 3) {
+            bytes_size = Integer.parseInt(arguments[1]);
+            loop_account = Integer.parseInt(arguments[2]);
+        }
+
+        for (int i = 0; i < loop_account; i++) {
+            byte[] rand_bytes = generateRandomArray(bytes_size);
+            try {
+                byte[] compressed = CompressionUtils.compress(rand_bytes);
+                print(String.format("%d origin length:%d, compressed length:%d, compressed rate:%.4f",
+                        i + 1, rand_bytes.length, compressed.length,
+                        (double)compressed.length / rand_bytes.length));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private static boolean is_invalid(String s) {
         return !s.isEmpty();
     }
@@ -381,5 +543,18 @@ public final class DhtShell {
         byte[] array = new byte[size];
         new Random().nextBytes(array);
         return array;
+    }
+
+    private static String getEntryType(Entry e) {
+        if (e == null || e.swig() == null) {
+            return "";
+        }
+
+        entry eswig = e.swig();
+        return eswig.type().toString();
+    }
+
+    private static boolean isEntryUndefined(Entry e) {
+        return sUndefinedEntry.equals(getEntryType(e));
     }
 }
--- src/main/java/io/tau/SessionSettings.java
+++ src/main/java/io/tau/SessionSettings.java
@@ -7,35 +7,55 @@ public final class SessionSettings {
 
     private static final int DHT_MAX_ITEMS = 1000000;
 
-    private static SessionParams sSessionParams;
+    private static settings_pack sp;
+
+    private static int sStarting_Port = 6881;
 
     static {
-        settings_pack sp = new settings_pack();
-	sp.set_str(settings_pack.string_types.dht_bootstrap_nodes.swigValue(), dhtBootstrapNodes());
-
-	session_params sparams = new session_params(sp);
-	dht_settings ds = new dht_settings();
-	ds.setMax_dht_items(DHT_MAX_ITEMS);
-	sparams.setDht_settings(ds);
-
-        sSessionParams = new SessionParams(sparams);
+        sp = new settings_pack();
+	    sp.set_str(settings_pack.string_types.dht_bootstrap_nodes.swigValue(), dhtBootstrapNodes());
     }
 
     public static SessionParams getTauSessionParams() {
-        return sSessionParams;
+        sp.set_str(settings_pack.string_types.listen_interfaces.swigValue(),
+                "0.0.0.0:" + sStarting_Port/*listenInterfaces()*/);
+        sStarting_Port++;
+
+        session_params sparams = new session_params(sp);
+        dht_settings ds = new dht_settings();
+        ds.setMax_dht_items(DHT_MAX_ITEMS);
+        sparams.setDht_settings(ds);
+
+        return new SessionParams(sparams);
     }
 
-    private static String dhtBootstrapNodes() {
+    public static String dhtBootstrapNodes() {
         StringBuilder sb = new StringBuilder();
 
         sb.append("dht.libtorrent.org:25401").append(",");
         sb.append("router.bittorrent.com:6881").append(",");
+        sb.append("router.utorrent.com:6881").append(",");
         sb.append("dht.transmissionbt.com:6881").append(",");
-	// tau nodes
-	sb.append("13.229.53.249:6881").append(",");
         // for DHT IPv6
         sb.append("router.silotis.us:6881");
+        //sb.append("tau.geekgalaxy.com:32777").append(",");
+        //sb.append("tau.geekgalaxy.com:32778");
 
         return sb.toString();
+    }
+
+    private static String listenInterfaces() {
+        String interfaces = "";
+        final int count = 1;
+        int port = 6881;
+
+        for (int i = 1; i < count; i++) {
+            interfaces = interfaces + "0.0.0.0:" + port + ",";
+            port++;
+        }
+
+        interfaces = interfaces + "0.0.0.0:" + port;
+
+        return interfaces;
     }
 }
